5fb9221ffb7b39697bf55c16fc68e3cc20ae62c4
simonvegedal <simonvegedal@gmail.com>
2026-01-20 22:23:09 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -64,8 +64,8 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
 
